21004cc9ff56c3e12cc2f2bdca1c5a09dbcbd571
Mohamed Mohamed <justmoe23@gmail.com>
2022-01-03 22:51:29 +0000

    create student and cohort tables

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,21 @@
+-- CREATING STUDENTS TABLE
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100),
+  phone VARCHAR(40),
+  github VARCHAR (255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+-- CREATING COHORTS TABLE
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
